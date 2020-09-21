27c4a84d2feb285656471f6b63c3060ca8908312
Henrique de Abreu Amitay <henrique.amitay@quintoandar.com.br>
2020-09-21 20:26:43 -0300

    Update execute.js

--- commands/execute.js
+++ commands/execute.js
@@ -23,7 +23,7 @@ const execute = async (message) => {
       voiceChannel: voiceChannel,
       connection: undefined,
       songs,
-      volume: 1,
+      volume: 0.2,
       playing: true,
     };
 
