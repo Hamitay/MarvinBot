3ab9e8fe26ea9eb3ac6e0b1ec1ef9f5f69a7f348
Hamitay <henrique.amitay@quintoandar.com.br>
2021-09-09 20:22:33 -0300

    Fixing playlist message

--- src/commands/playlist/messages.ts
+++ src/commands/playlist/messages.ts
@@ -1,6 +1,6 @@
 const messages = {
   PLAYING_PLAYLIST: (title: string) =>
-    `Do you really the ${title} playlist, it is terrible`,
+    `Do you really the want the ${title} playlist, it is terrible`,
 } as const;
 
 export default messages;
