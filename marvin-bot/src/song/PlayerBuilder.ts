0c2242db3576707f303ab9d971e4b9b079efb85d
Hamitay <henriqueamitay@gmail.com>
2023-04-06 20:38:52 -0300

    Added pino logs to playerBuilder

--- marvin-bot/src/song/PlayerBuilder.ts
+++ marvin-bot/src/song/PlayerBuilder.ts
@@ -1,22 +1,25 @@
 import { Player } from "discord-player";
 import { Client } from "discord.js";
+import pino from "pino";
 
 export const buildPlayer = (client: Client) => {
   const player = Player.singleton(client, {
     ytdlOptions: { quality: "highestaudio", highWaterMark: 1 << 25 },
   });
 
+  const logger = pino();
+
   //Add player events here
   player.on("debug", async (message) => {
     // Emitted when the player sends debug info
     // Useful for seeing what dependencies, extractors, etc are loaded
-    console.log(`General player debug event: ${message}`);
+    logger.info(`General player debug event: ${message}`);
   });
 
   player.events.on("debug", async (queue, message) => {
     // Emitted when the player queue sends debug info
     // Useful for seeing what state the current queue is at
-    console.log(`Player debug event: ${message}`);
+    logger.info(`Player debug event: ${message}`);
   });
 
   return player;
